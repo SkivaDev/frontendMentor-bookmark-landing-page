b746bad8af4c151ee5af54eef17262ef59794beb
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-31 19:03:04 -0500

    feat(containers): :sparkles: Add HeroSection HTML structure, and Tailwind styles.

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Layout from "../containers/Layout";
+import HeroSection from "../containers/HeroSection";
 
 function Home() {
   return (
     <>
       <Layout>
-        
+        <main>
+          <HeroSection />
+        </main>
       </Layout>
     </>
   );
