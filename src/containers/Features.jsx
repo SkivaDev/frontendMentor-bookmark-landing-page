fa917bd72ad8187cf28e9b4865ee9d3cb7825234
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-01 17:43:54 -0500

    feat(containers): :lipstick: add responsive design to Features.jsx

--- src/containers/Features.jsx
+++ src/containers/Features.jsx
@@ -11,22 +11,21 @@ function Features() {
   const [featureNum, setFeatureNum] = useState(1);
 
   return (
-    <div className="flex flex-col items-center justify-center max-w-[73.375rem] mx-auto mt-[6.875rem]  md:gap-16 md:px-8">
+    <div className="flex flex-col items-center justify-center max-w-[73.375rem] mx-auto mt-[6.875rem] px-[1.25rem] gap-[2.875rem] md:px-8">
       <div className="flex flex-col gap-[1.25rem] items-center max-w-[33.575rem]">
-        <h2 className="text-[2rem] font-bold">Features</h2>
-        <p className="text-center text-[1.125rem] text-grayish-blue">
+        <h2 className="text-[1.5rem] md:text-[2rem] font-medium">Features</h2>
+        <p className="text-center md:text-[1.125rem] text-grayish-blue">
           Our aim is to make it quick and easy for you to access your favourite
           websites. Your bookmarks sync between your devices so you can access
           them on the go.
         </p>
       </div>
       <div className="flex flex-col items-center justify-center w-full">
-        <div className="flex items-center justify-center w-full">
+        <div className="flex flex-col md:flex-row items-center justify-center w-full gap-4 md:gap-0">
           {featuresButtons.map((btn, i) => (
             <button
               key={i}
               onClick={() => setFeatureNum(i + 1)}
-              // className=" px-[2.6875rem] py-[1.625rem] text-very-dark-blue/50"
               className={`border-tab relative min-w-[15.2325rem] border-b-2 px-6 py-4 tracking-[0.04em] text-very-dark-blue/50 transition duration-300 hover:text-soft-red md:px-[2.6875rem] md:py-[1.625rem] 
               ${(featureNum === i + 1) ? "border-selected" : ""}
               `}
@@ -35,21 +34,21 @@ function Features() {
             </button>
           ))}
         </div>
-        <div className="flex flex-col items-center justify-center md:justify-between md:flex-row mx-auto mt-[3.5625rem] max-w-[73.375rem] md:gap-16 md:px-8">
-          <div>
+        <div className="flex flex-col items-center justify-center md:justify-between lg:items-start md:flex-row mx-auto max-w-[73.375rem]   gap-16 py-[4.3125rem] banner-2">
+          <div className="flex-[1.15] width-fill-available max-w-[33.625rem] md:max-w-full">
             <img
               src={features[featureNum - 1].imageUrl}
               alt="feature-image"
               aria-hidden="true"
-              width={657}
-              height={466}
-              className="mx-auto rwd:max-w-[125%]"
+              width={538}
+              height={347}
+              className=""
             />
           </div>
-          <div>
-            <h3>{features[featureNum - 1].title}</h3>
-            <p>{features[featureNum - 1].description}</p>
-            <button>{features[featureNum - 1].buttonText}</button>
+          <div className="flex-1 flex flex-col gap-[1.5625rem] md:items-start lg:pl-[4.5rem] lg:pt-[3.25rem] text-center md:text-left">
+            <h3 className="text-[1.5rem] md:text-[2rem] font-medium ">{features[featureNum - 1].title}</h3>
+            <p className="text-[1.125rem] text-grayish-blue">{features[featureNum - 1].description}</p>
+            <button className="text-[.875rem] rounded-md border-[0.188rem] border-soft-blue bg-soft-blue py-[0.655rem] px-[1.521rem] font-medium text-white transition duration-300 hover:bg-white hover:text-soft-blue mx-auto md:mx-0">{features[featureNum - 1].buttonText}</button>
           </div>
         </div>
       </div>
