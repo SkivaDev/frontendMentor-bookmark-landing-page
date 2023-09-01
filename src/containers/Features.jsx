6610d4069624f2954d85c54be31860b28778ddf8
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-31 19:33:15 -0500

    feat(data): :art: Update features.json data

--- src/containers/Features.jsx
+++ src/containers/Features.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function Features() {
+
+  const featuresButtons = ['Simple Bookmarking', 'Speedy Searching', 'Easy Sharing']
+
   return (
     <div className="">
       <div>
-        <h2></h2>
-        <p></p>
+        <h2>Features</h2>
+        <p>Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
       </div>
       <div>
         <div>
