c3f51c8250d46d9af863e2699b42bce0ac36095b
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-31 19:57:30 -0500

    feat(containers): :art: Udd text and structure to Features.jsx, and update features.json data

--- src/containers/Features.jsx
+++ src/containers/Features.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { features } from "../data/features.json";
 
 function Features() {
+  const featuresButtons = [
+    "Simple Bookmarking",
+    "Speedy Searching",
+    "Easy Sharing",
+  ];
 
-  const featuresButtons = ['Simple Bookmarking', 'Speedy Searching', 'Easy Sharing']
+  const [featureNum, setFeatureNum] = useState(1);
+
+
+
+
 
   return (
     <div className="">
       <div>
         <h2>Features</h2>
-        <p>Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
+        <p>
+          Our aim is to make it quick and easy for you to access your favourite
+          websites. Your bookmarks sync between your devices so you can access
+          them on the go.
+        </p>
       </div>
       <div>
         <div>
-          <button></button>
-          <button></button>
-          <button></button>
+          {featuresButtons.map((btn, i) => (
+            <button key={i} onClick={() => setFeatureNum(i + 1)}>{btn}</button>
+          ))}
         </div>
         <div>
           <div>
             <img
-              src={hero}
-              alt="hero-image"
+              src={features[featureNum - 1].imageUrl}
+              alt="feature-image"
               aria-hidden="true"
               width={657}
               height={466}
@@ -28,9 +42,9 @@ function Features() {
             />
           </div>
           <div>
-            <h3></h3>
-            <p></p>
-            <button></button>
+            <h3>{features[featureNum - 1].title}</h3>
+            <p>{features[featureNum - 1].description}</p>
+            <button>{features[featureNum - 1].buttonText}</button>
           </div>
         </div>
       </div>
