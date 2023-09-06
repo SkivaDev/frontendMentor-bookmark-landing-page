b3cc792acbc947cd1551849862d20d5cf7d98ccf
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-06 18:19:49 -0500

    style(containers): :lipstick: fix text center at ContainerText from FAQSection

--- src/containers/FAQSection.jsx
+++ src/containers/FAQSection.jsx
@@ -17,7 +17,7 @@ function FAQSection() {
 function ContainerText() {
   return (
     <div className="flex flex-col gap-[1.25rem] items-center max-w-[33.575rem]">
-      <h2 className="text-[1.5rem] md:text-[2rem] font-medium">
+      <h2 className="text-center text-[1.5rem] md:text-[2rem] font-medium">
         Frequently Asked Questions
       </h2>
       <p className="text-center md:text-[1.125rem] text-grayish-blue">
